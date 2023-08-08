91c25711141b40732a87983e3b73d663a6fb877d
Peter Siegmund <developer@mars3142.org>
2023-08-08 09:21:28 +0200

    add AGPL license

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
+//----------------------------------------------------------------------------
+// OS-Railway - Remote Control
+// Copyright (C) 2023 Peter Siegmund (https://mars3142.dev)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------------
 #include <Arduino.h>
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
